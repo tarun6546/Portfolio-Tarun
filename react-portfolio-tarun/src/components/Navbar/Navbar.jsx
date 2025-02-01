e420609fb66f1975a309cc281921049c77705124
tarun6546 <tarunvarshney2112@gmail.com>
2025-02-01 23:43:11 +0530

    i am completing my navbar setions but the issue is menuicon is not working properly

--- react-portfolio-tarun/src/components/Navbar/Navbar.jsx
+++ react-portfolio-tarun/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import styles from './Navbar.module.css';
+import React, { useState } from "react";
 
-const Navbar = () => {
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="/">PortFolio</a>
-        <div className={styles.menu}>
-            <ul className={styles.menuItems}>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
