4d22bd386ff1fcf839ba709615bc22fbf59a4fa3
tarun6546 <tarunvarshney2112@gmail.com>
2025-02-06 00:15:39 +0530

    about section ready

--- react-portfolio-tarun/src/App.jsx
+++ react-portfolio-tarun/src/App.jsx
@@ -2,6 +2,7 @@ import react from 'react'
 import styles from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import About from './components/About/About'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className={styles.App}>
         <Navbar />
         <Hero />
+        <About />
       </div>;
     </>
   )
